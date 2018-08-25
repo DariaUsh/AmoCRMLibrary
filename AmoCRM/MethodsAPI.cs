d593898cb876e4f319bb6b77b3f84546273b49ca
DariaUsh <begemoticdasha@yandex.ru>
2018-08-25 15:10:32 +0300

    Добавьте файлы проекта.

---
+++ AmoCRM/MethodsAPI.cs
@@ -0,0 +1,219 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Xml;
+
+namespace AmoCRM
+{
+    public class MethodsAPI
+    {
+        public enum TaskType
+        {
+            ring = 1,
+            meeting,
+            writeAletter
+        }
+
+        public enum ElementType
+        {
+            contact = 1,
+            deal = 2,
+            company = 3,
+            buyer = 12
+        }
+        public static CookieCollection Authorize(string Subdomen, string UserLogin, string UserHash)
+        {
+            try
+            {
+                var URL = "https://" + Subdomen + "/private/api/auth.php";
+                var parametrs = "USER_LOGIN=" + UserLogin + "&USER_HASH=" + UserHash;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.CookieContainer = new CookieContainer();
+
+                byte[] EncodedPostParams = Encoding.GetEncoding(1251).GetBytes(parametrs);
+                request.ContentLength = EncodedPostParams.Length;
+                request.GetRequestStream().Write(EncodedPostParams, 0, EncodedPostParams.Length);
+                request.GetRequestStream().Close();
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+                XmlDocument docXML = new XmlDocument();
+                docXML.LoadXml(responseString);
+                var auth = docXML.DocumentElement.ChildNodes[0].InnerText == "true";
+
+                if (auth)
+                {
+                    return request.CookieContainer.GetCookies(request.RequestUri);
+                }
+                else
+                {
+                    return new CookieCollection();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        public static List<int> AddTask(CookieCollection cookie, string subdomen, List<TaskCRM> TaskList)
+        {
+            var URL = "https://" + subdomen + "/api/v2/tasks";
+            List<int> idList = new List<int>();
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.CookieContainer = new CookieContainer();
+                httpWebRequest.CookieContainer.Add(cookie);
+
+                string dataJson = "{\"add\": [";
+                foreach (TaskCRM item in TaskList)
+                {
+                    dataJson += item.SerializeItem();
+                }
+                dataJson = dataJson.Replace("}{", "},{") + "]}";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    streamWriter.Write(dataJson);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    if (!String.IsNullOrEmpty(result))
+                    {
+                        JObject jObject = JObject.Parse(result);
+                        var items = jObject["_embedded"]["items"];
+                        foreach (JToken token in items)
+                        {
+                            idList.Add(token["id"].ToObject<int>());
+                        }
+                    }
+                }
+                return idList;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static List<TaskCRM> GetTasks(CookieCollection cookie, string subdomen)
+        {
+            try
+            {
+                var URL = "https://" + subdomen + "/api/v2/tasks";
+                return GetTaskList(cookie, URL);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static List<TaskCRM> GetTasks(CookieCollection cookie, string subdomen, int idTask, int element_id,
+                                                                  int responsible_user_id, int element_type)
+        {
+            try
+            {
+                var URL = "https://" + subdomen + "/api/v2/tasks?id=" + idTask + "&element_id=" + element_id +
+                          "&responsible_user_id=" + responsible_user_id + "&element_type=" + element_type;
+                return GetTaskList(cookie, URL);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public static List<TaskCRM> GetTasks(CookieCollection cookie, string subdomen, int idTask, int limit_rows, int limit_offset,
+                                                                    int element_id, int responsible_user_id, int element_type)
+        {
+            try
+            {
+                var URL = "https://" + subdomen + "/api/v2/tasks?id =" + idTask + "&limit_rows=" + limit_rows + "&limit_offset="
+                        + limit_offset + "&element_id=" + element_id + "&responsible_user_id=" + responsible_user_id + "&element_type=" + element_type;
+                return GetTaskList(cookie, URL);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        static List<TaskCRM> GetTaskList(CookieCollection cookie, string URL)
+        {
+            List<TaskCRM> taskList = new List<TaskCRM>();
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(URL);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.CookieContainer = new CookieContainer();
+            httpWebRequest.CookieContainer.Add(cookie);
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                if (!String.IsNullOrEmpty(result))
+                {
+                    JObject jObject = JObject.Parse(result);
+                    var items = jObject["_embedded"]["items"];
+                    foreach (JToken item in items)
+                    {
+                        taskList.Add(new JavaScriptSerializer().Deserialize<TaskCRM>(JsonConvert.SerializeObject(item)));
+                    }
+                }
+
+            }
+            return taskList;
+        }
+
+        public static DateTime ToDateTime(long timestamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            if (timestamp == 0)
+            {
+                return new DateTime();
+            }
+            return dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+        }
+
+        public static long ToTimeStamp(DateTime date)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            if (date == DateTime.MinValue)
+            {
+                return 0;
+            }
+            return (long)Math.Floor((date.ToUniversalTime() - unixStart).TotalSeconds);
+        }
+
+        public static long DateTimeToTimeStamp(DateTime date, TimeSpan time)
+        {
+            DateTime unixStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            if (date == DateTime.MinValue)
+            {
+                return 0;
+            }
+            date.Add(time);
+            return (long)Math.Floor((date.ToUniversalTime() - unixStart).TotalSeconds);
+
+        }
+    }
+}
---
+++ AmoCRM/TaskCRM.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmoCRM
+{
+    public class TaskCRM
+    {
+        public int id { get; set; }
+        public int account_id { get; set; }
+        public int group_id { get; set; }
+        public int element_id { get; set; }
+        public MethodsAPI.ElementType element_type { get; set; }
+        public long complete_till_at { get; set; }
+        public MethodsAPI.TaskType task_type { get; set; }
+        public string text { get; set; }
+        public long created_at { get; set; }
+        public long updated_at { get; set; }
+        public bool is_completed { get; set; }
+        public int responsible_user_id { get; set; }
+        public int created_by { get; set; }
+
+        object _links { get; set; }
+        string[] result { get; set; }
+
+        public TaskCRM()
+        {
+
+        }
+
+        public TaskCRM(int element_id, MethodsAPI.ElementType element_type, long complete_till_at, MethodsAPI.TaskType task_type, string text,
+                long created_at, long updated_at, bool is_completed, int responsible_user_id, int created_by)
+        {
+            DateTime unixStart = DateTime.SpecifyKind(new DateTime(1970, 1, 1), DateTimeKind.Utc);
+            this.element_id = element_id;
+            this.element_type = element_type;
+            this.complete_till_at = complete_till_at;
+            this.task_type = task_type;
+            this.text = text;
+            this.created_at = created_at;
+            this.updated_at = updated_at;
+            this.is_completed = is_completed;
+            this.responsible_user_id = responsible_user_id;
+            this.created_by = created_by;
+        }
+
+        public string SerializeItem()
+        {
+            string jsonItem = "{\"element_id\":" + this.element_id + ","
+                     + "\"element_type\":" + (int)this.element_type + ","
+                     + "\"complete_till_at\":" + this.complete_till_at + ","
+                     + "\"task_type\":" + (int)this.task_type + ","
+                     + "\"text\":\"" + this.text + "\","
+                     + "\"created_at\":" + this.created_at + ","
+                     + "\"updated_at\":" + this.updated_at + ","
+                     + "\"responsible_user_id\":" + this.responsible_user_id + ","
+                     + "\"created_by\":" + this.created_by + "}";
+            return jsonItem;
+        }
+    }
+}
